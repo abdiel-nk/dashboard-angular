35757480091fa61b1d5900aaa643ebee12cb905e
unknown <abdielmamani@upeu.edu.pe>
2025-08-21 16:56:28 -0500

    estructura

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,65 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CustomSidenav } from "./component/custom-sidenav/custom-sidenav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
+  standalone: true,
+  imports: [CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    CustomSidenav],
 
-    <router-outlet />
+  template: `
+    <mat-toolbar class="mat-elevation-z3">
+      <button mat-icon-button (click)="collapsed.set(!collapsed())">
+        <mat-icon>menu</mat-icon>
+      </button>
+    </mat-toolbar>
+
+    <mat-sidenav-container>
+      <mat-sidenav opened mode="side" [style.width]="sidenavWidth()">
+        <app-custom-sidenav [collapsed]="collapsed()" />
+      </mat-sidenav>
+
+      <mat-sidenav-content class="content" [style.margin-left]="sidenavWidth()">
+        <router-outlet></router-outlet>
+      </mat-sidenav-content>
+
+    </mat-sidenav-container>
   `,
-  styles: [],
+  styles: [
+    `
+    mat-toolbar{
+      position: relative;
+      z-index:5;
+    }
+
+    .content {
+      padding: 24px;
+    }
+    mat-sidenav-container{
+      height: calc(100vh - 64px);
+    }
+
+    `
+  ],
 })
+
 export class App {
   protected readonly title = signal('youtube-sidebar');
+
+  collapsed = signal(false);
+
+  sidenavWidth = computed(()=> this.collapsed()? '65px':'250px');
+
+
 }
---
+++ src/app/component/custom-sidenav/custom-sidenav.ts
@@ -0,0 +1,109 @@
+import { Component, computed, Input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+export type MenuItem = {
+  icon:string;
+  label:string; 
+  route:string;
+};
+
+@Component({
+  selector: 'app-custom-sidenav',
+  standalone : true,
+  imports: [CommonModule, MatListModule, MatIconModule, RouterModule],
+  template: `
+    <div class="sidenav-header">
+      <img 
+        [width]="profilePicSize()" 
+        [height]="profilePicSize()" 
+        src="https://diario.infoperiodistas.info/wp-content/uploads/2025/08/La-vuelta-ciclista-2025.jpg" alt="img-logo">
+      <div class="header-text" [class.hide-header-text]="sideNavCollapsed()">
+        <h2>Your channel</h2>
+        <p>Colnago2 Statics</p>
+      </div>
+    </div>
+
+    <mat-nav-list>
+      <a mat-list-item *ngFor="let item of menuItems()" [routerLink]="item.route">
+        <mat-icon matListItemIcon >{{item.icon}}</mat-icon>
+        <span matListItemTitle *ngIf="!sideNavCollapsed()" >{{item.label}}</span>
+      </a>
+    </mat-nav-list>
+  `,
+  styles: [
+    
+    `
+    .sidenav-header{
+      padding-top:24px;
+      text-align:center;
+      margin:0;
+      > img{
+        border-radius:100%;
+        object-fit:cover;
+        margin-bottom: 10px;
+      }
+      .header-text{
+        > h2{
+          margin:0;
+          font-size: 1rem;
+          line-height: 1.5rem;
+        }
+        >p{
+          margin:0;
+          font-size: 0.8rem;
+        }
+      }
+    }
+    .hide-header-text{
+      opacity:0;
+      height: 0px;
+    }
+
+    `
+  ]
+})
+export class CustomSidenav {
+
+  
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard',
+
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content',
+
+    },
+    {
+      icon: 'analytics',
+      label: 'Analytics',
+      route: 'analytics',
+
+    },
+    {
+      icon: 'comment',
+      label: 'Comments',
+      route: 'comments',
+
+    },
+    
+    
+  ]);
+
+
+  sideNavCollapsed = signal(false);
+    @Input() set collapsed(val: boolean){
+    this.sideNavCollapsed.set(val);
+  }
+
+  profilePicSize = computed(() => this.sideNavCollapsed() ? '32' : '100');
+
+}
